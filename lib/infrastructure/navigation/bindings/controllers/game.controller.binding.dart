3536d8b9a06bd045260d1c4b5646d3ba8dbdbe45
Soejii <ninkaishou27@gmail.com>
2025-11-25 22:28:29 +0700

    fix: fixed bad import in controller bindings

--- lib/infrastructure/navigation/bindings/controllers/game.controller.binding.dart
+++ lib/infrastructure/navigation/bindings/controllers/game.controller.binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
+import 'package:pulse_flow/presentation/game/controllers/game.controller.dart';
 
-import '../../../../presentation/game/controllers/game.controller.dart';
 
 class GameControllerBinding extends Bindings {
   @override
