b36818048c548837f60e806975ab57777a0eabff
Soejii <ninkaishou27@gmail.com>
2025-12-15 01:24:29 +0700

    chore: updating web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4112403952"
+    serviceWorkerVersion: "2581238093"
   }
 });
